3bffd1d17f1d1560f6d0321acedf4fbabde646ab
YoYoMo111 <you.mo@ufl.edu>
2016-03-22 19:20:49 -0400

    fix menu highlight

--- quincy/chantapp.js
+++ quincy/chantapp.js
@@ -18,6 +18,7 @@ function ChantApp(container_div_id) {
 	
 	this.lastClickedItem = 0;
 }
+var groupNum;
 
 //NOTE: For St. Gall school = 1, for Laon school = 2
 ChantApp.prototype.initLesson = function(school, level, group) {
@@ -28,16 +29,23 @@ ChantApp.prototype.initLesson = function(school, level, group) {
 	
 	this.lesson = new Lesson(this.school, level, group);
     this.lesson.initUI(this.container_div_id);
-    
+
+    if (level==1||level==2||level==6){groupNum=5;}
+    else if(level==3){groupNum=2;}
+    else if(level==4){groupNum=7;}
+    else if(level==5){groupNum=4;}
+
 	// Change text color of selected menu item to red
-	for (var i = 1; i <= 5; i++){
-		document.getElementById("menu-"+school+"-"+level+"-"+i).style.background="transparent";//yoyo
+	for (var i = 1; i <= groupNum; i++){
+		//document.getElementById("menu-"+school+"-"+level+"-"+i).style.background="transparent";//yoyo
+		document.getElementById("menu-"+i).style.background="transparent";//yoyo
 	}
 	document.getElementById("u3339").style.background="transparent";//intro
 	document.getElementById("u1981").style.background="transparent";//review
 	document.getElementById("u1960").style.background="transparent";//assessment
 	document.getElementById("quizlet").style.background="transparent";//quizlet
-	document.getElementById("menu-"+school+"-"+level+"-"+group).style.background="#FAA03F";//yoyo
+	//document.getElementById("menu-"+school+"-"+level+"-"+group).style.background="#FF994D";//yoyo
+	document.getElementById("menu-"+group).style.background="#FF994D";//yoyo
 	
 	
 //	this.changeTextColor(group);
@@ -59,12 +67,18 @@ ChantApp.prototype.initReviewLesson = function(school, level) {
 	
 	this.reviewLesson.initUI(this.container_div_id);
 
+	if (level==1||level==2||level==6){groupNum=5;}
+    else if(level==3){groupNum=2;}
+    else if(level==4){groupNum=7;}
+    else if(level==5){groupNum=4;}
+
 	// Change text color of selected menu item to red
-	for (var i = 1; i <= 5; i++){
-		document.getElementById("menu-"+school+"-"+level+"-"+i).style.background="transparent";//yoyo
+	for (var i = 1; i <= groupNum; i++){
+		//document.getElementById("menu-"+school+"-"+level+"-"+i).style.background="transparent";//yoyo
+		document.getElementById("menu-"+i).style.background="transparent";//yoyo
 	}
 	document.getElementById("u3339").style.background="transparent";//intro
-	document.getElementById("u1981").style.background="#FAA03F";//review
+	document.getElementById("u1981").style.background="#FF994D";//review
 	document.getElementById("u1960").style.background="transparent";//assessment
 	document.getElementById("quizlet").style.background="transparent";//quizlet
 
@@ -86,15 +100,21 @@ ChantApp.prototype.initTest = function(school, level) {
 	
 	this.levelTest = new LevelTest(this.school, level, this.stGallGroups[level-1]);
 	this.levelTest.initUI(this.container_div_id);
+
+	if (level==1||level==2||level==6){groupNum=5;}
+    else if(level==3){groupNum=2;}
+    else if(level==4){groupNum=7;}
+    else if(level==5){groupNum=4;}
 	
 	// Change text color of selected menu item to red
 
-	for (var i = 1; i <= 5; i++){
-		document.getElementById("menu-"+school+"-"+level+"-"+i).style.background="transparent";//yoyo
+	for (var i = 1; i <= groupNum; i++){
+		//document.getElementById("menu-"+school+"-"+level+"-"+i).style.background="transparent";//yoyo
+		document.getElementById("menu-"+i).style.background="transparent";//yoyo
 	}
 	document.getElementById("u3339").style.background="transparent";//intro
 	document.getElementById("u1981").style.background="transparent";//review
-	document.getElementById("u1960").style.background="#FAA03F";//assessment
+	document.getElementById("u1960").style.background="#FF994D";//assessment
 	document.getElementById("quizlet").style.background="transparent";//quizlet
 
 /*	var count = 0;
@@ -115,15 +135,21 @@ ChantApp.prototype.initQuizlet = function(school, level) {
 	
 	this.quizlet = new Quizlet(this.school, level, this.stGallGroups[level-1]);
 	this.quizlet.initUI(this.container_div_id);
+
+	if (level==1||level==2||level==6){groupNum=5;}
+    else if(level==3){groupNum=2;}
+    else if(level==4){groupNum=7;}
+    else if(level==5){groupNum=4;}
 	
 	// Change text color of selected menu item to red
-	for (var i = 1; i <= 5; i++){
-		document.getElementById("menu-"+school+"-"+level+"-"+i).style.background="transparent";//yoyo
+	for (var i = 1; i <= groupNum; i++){
+		//document.getElementById("menu-"+school+"-"+level+"-"+i).style.background="transparent";//yoyo
+		document.getElementById("menu-"+i).style.background="transparent";//yoyo
 	}
 	document.getElementById("u3339").style.background="transparent";//intro
 	document.getElementById("u1981").style.background="transparent";//review
 	document.getElementById("u1960").style.background="transparent";//assessment
-	document.getElementById("quizlet").style.background="#FAA03F";//quizlet
+	document.getElementById("quizlet").style.background="#FF994D";//quizlet
 
 /*	var count = 0;
 	if (this.school == "StGall") {
