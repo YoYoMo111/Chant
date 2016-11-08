08f8ace4f86f8dd500e09beb20cd447329094f8a
YoYoMo111 <you.mo@ufl.edu>
2016-11-08 10:50:11 -0500

    embed quizlet

--- quincy/quizlet.js
+++ quincy/quizlet.js
@@ -8,11 +8,11 @@ function Quizlet(school,level,group){
 Quizlet.prototype.initUI = function(container_div_id) {
 	document.getElementById("scatter").innerHTML =
 	'<a href="javascript:void(0)" onclick="document.getElementById(\'scatter\').style.display=\'none\';document.getElementById(\'fade\').style.display=\'none\'" class="textright">Close</a>'+
-  	'<p><iframe src="https://quizlet.com/97373929/scatter/embed" height="550" width="100%" style="border:0"></iframe></p>';
+  	'<p><iframe src="https://quizlet.com/156945177/scatter/embed" height="550" width="100%" style="border:0"></iframe></p>';
 
   	document.getElementById("race").innerHTML =
   	'<a href="javascript:void(0)" onclick="document.getElementById(\'race\').style.display=\'none\';document.getElementById(\'fade\').style.display=\'none\'" class="textright">Close</a>'+
-  	'<p><iframe src="https://quizlet.com/97373929/spacerace/embed" height="550" width="100%" style="border:0"></iframe></p>';
+  	'<p><iframe src="https://quizlet.com/156945177/gravity/embed" height="550" width="100%" style="border:0"></iframe></p>';
 
 	this.container_div = document.getElementById(container_div_id);
 	this.container_div.innerHTML = 
